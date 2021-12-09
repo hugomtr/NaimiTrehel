334bde1b7f7d9b40fba6e39af97f91e9ea702d69
hugomtr <hugomaitre340@gmail.com>
2021-12-09 15:57:30 +0100

    some correction

--- main.c
+++ main.c
@@ -9,7 +9,10 @@
 
 /// VA globales
 int n_site = 3;
-
+int MAX_SIZE = 1000;
+int LENGTH_MAX = 3000;
+int neighbors [3000]; 
+int root_port;
 
 int connexion(int port){
     int ds = socket(AF_INET, SOCK_STREAM, 0);
@@ -43,21 +46,58 @@ void * acceptneighbors(void* socket){
     socklen_t t = sizeof (struct sockaddr_in);
     while(1)
     {
-        int firstSocket = accept(*ptrmainsocket,(struct sockaddr *)addrNeighborsAccepted, &t);    
-        
+        int firstSocketconnexion = accept(*ptrmainsocket,(struct sockaddr *)&addrNeighborsAccepted, &t);
+        if (firstSocketconnexion>0){
+            printf("connexion accepté par le socket principal");
+            char buffer[MAX_SIZE];
+            recv(firstSocketconnexion,buffer,sizeof(buffer),0);// receive data from the socket connected
+        }   
+
     }
 }
 
-void * connectneighbors(void* s)
+void * connectneighbors(void* general_port)
 {
-    
+    int* port = (int *)general_port;
+    for (int curr_port = *port; curr_port<= *port + n_site; curr_port++)
+    {
+        if (neighbors[curr_port] == -1 && curr_port != root_port)
+        {
+            neighbors[curr_port] = socket(AF_INET, SOCK_STREAM, 0);
+            
+            if (neighbors[curr_port] == -1){
+                printf("Client : pb creation socket\n");
+                exit(1);
+            }
+
+            printf("Création de la socket\n");
+            
+            struct sockaddr_in adrServ;
+            adrServ.sin_family = AF_INET;
+            adrServ.sin_addr.s_addr = inet_addr("127.0.0.1");
+            adrServ.sin_port = htons(curr_port);
+
+            int lgAdr = sizeof(struct sockaddr_in);
+
+            int conn = connect(neighbors[curr_port], (struct sockaddr *)&adrServ,sizeof(adrServ));
+
+            if (conn < 0) 
+            {
+                neighbors[curr_port] = -1;
+            } else {
+                char buffer[MAX_SIZE];
+                send(conn,)
+            }
+
+        }
+    }
 }
 
 
 int main(int argc, char *argv[])
 {
     if (argc<2){
-        printf("utilisation: %s numero_port_main_socket \n", argv[0]);
+        printf("utilisation: %s numero_port_main_socket entre 2000 et 2900 \n", argv[0]);
         exit(1);
     }
 
@@ -66,16 +106,16 @@ int main(int argc, char *argv[])
     int general_sock = connexion(general_port);
 
     int neighbors[general_port + n_site];
-    for (int i = 0; i < n_site; i++){
+    for (int i = 0; i < LENGTH_MAX; i++){
         neighbors[i] = -1;
     }
-
 
     pthread_t acceptConnexion;
     pthread_create(&acceptConnexion,NULL,acceptneighbors,&general_sock);
 
     pthread_t connectAllSite;
-    pthread_create(&connectAllSite,NULL,connectneighbors, NULL);
+    pthread_create(&connectAllSite,NULL,connectneighbors,&general_port);
+
 
 
     return 0;
