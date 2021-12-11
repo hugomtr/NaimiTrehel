e17fb1c68ad971178a3466509257dc168ae8f200
hugomtr <hugomaitre340@gmail.com>
2021-12-11 10:13:11 +0100

    run function completion

--- main.cpp
+++ main.cpp
@@ -15,12 +15,68 @@ Authors:
 #include <pthread.h>
 #include "main.h"
 
+
 int writing_in_files(FILE * fp){
     for (int i =0;i<1200;i++){
         fprintf(fp,"Hey bro Thread :%d \n",i);
     }
     return 0;
 }
+
+void * traitement_message(void * params){
+    threadArgs *arg = (struct threadArgs *)params;
+    printf("myId %d\n", arg->myID);
+    printf("myPort %d\n", arg->myPort);
+    printf("last %d\n", *(arg->last));
+    printf("next %d\n", *(arg->next));
+    return NULL;
+}
+
+void * travail(void * args){
+    return 0;
+}
+
+
+int run(int racine_last_id,FILE * fp,int start_port){
+    fprintf(fp,"Strart runnning Simulations of Naimi Trehel's Algorithm with %d nodes",NB_NODES);
+
+    int last[NB_NODES];
+    int next[NB_NODES]; 
+    int requete_CS[NB_NODES]; 
+    int token_present[NB_NODES]; 
+    
+    for (int i = 0;i<NB_NODES;i++){
+        last[i] = racine_last_id;
+        next[i] = NIL_PROCESS;
+        requete_CS[i] = 0;
+        token_present[i] = 0;
+    }
+    token_present[racine_last_id] = 1;
+
+    pthread_t thread_receveur[NB_NODES];
+    pthread_t thread_travailleur[NB_NODES];
+
+    threadArgs arg[NB_NODES];
+
+    for (int i = 0;i<NB_NODES;i++){
+        arg[i] = {.myID = i, .myPort = start_port + i ,.last = last+i, .next = next+i , .requete_CS = requete_CS+i, .token_present = token_present+i};
+    }
+    
+    for (int i = 0;i<NB_NODES;i++){
+        pthread_create(&thread_receveur[i],NULL,traitement_message,&arg[i]);
+    }
+
+    for (int i = 0;i<NB_NODES;i++){
+        pthread_create(&thread_travailleur[i],NULL,travail,NULL);
+    }
+
+    for (int i = 0;i<NB_NODES;i++){
+        pthread_join(thread_receveur[i],NULL);
+        pthread_join(thread_travailleur[i],NULL);
+    }
+    return 0;
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -43,7 +99,9 @@ int main(int argc, char *argv[])
         start_port = atoi(argv[2]);
     }
 
-    writing_in_files(fp);
+    int nodeIdInitialJeton = 0;
+    run(nodeIdInitialJeton,fp,start_port);
+
     fclose(fp);
 
     return 0;
--- main.h
+++ main.h
@@ -1,13 +1,25 @@
 #ifndef NB_NODES
 #define NB_NODES 5
+#define NIL_PROCESS -1
+
+enum messageType {
+    TOKEN,
+    REQUEST,
+};
+
+struct message {
+    enum messageType type;
+    int idEnvoyeur;
+    int idRequest;
+};
 
 struct threadArgs {
     int myID;
     int myPort;
-    int &last;
-    int &next;
-    int &requete_CS;
-    int &token_present;
+    int * last;
+    int * next;
+    int * requete_CS;
+    int * token_present;
     /* .... */
 };
 
@@ -17,6 +29,6 @@ bool desireSortirSC(int myID, int &requete_CS, int &next, int &last, int &has_to
 bool desireRentrerenSC(int myID, int &requete_CS, int &next, int &last, int &has_token, pthread_mutex_t *m_sendrec);
 void * travail(void * s);
 void * traitement_message(void * s);
-void run(int nodeInitialJeton);
+void run(int nodeInitialJeton, FILE file, int start_port);
 
 #endif
