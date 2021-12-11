124f9e7bbf8d4331d655ffc1ace9b80af9db31bf
adlina1 <adrien28210@gmail.com>
2021-12-11 09:52:48 +0100

    add of a function to create a receiver socket

--- main.cpp
+++ main.cpp
@@ -14,6 +14,11 @@ Authors:
 #include <stdlib.h>
 #include <pthread.h>
 #include "main.h"
+#include <string.h>
+#include <errno.h>
+#include <netinet/in.h>
+
+#define NB_SITES_MAX
 
 int writing_in_files(FILE * fp){
     for (int i =0;i<1200;i++){
@@ -21,6 +26,41 @@ int writing_in_files(FILE * fp){
     }
     return 0;
 }
+
+
+// socket serveur qui permettra la récéption des messages des autres sites
+int creerSocketReceveuse(int myID, int myPort){
+    printf("Le Site n°%d créé une socket receveuse n°%hu...", myID, myPort);
+    struct sockaddr_in server;
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(myPort);
+
+    // on créé la socket receveuse 
+    int sockfd;
+    if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0){
+        printf("Erreur : Site n°%d : Erreur lors de la création de la socket. Cause : ",myID);
+        perror("Socket()");
+        exit(1);
+    }
+
+    // on attache la socket à un port et une adresse
+    if (bind(sockfd, (struct sockaddr *)&server, sizeof(server)) < 0){
+        printf("Erreur : Site n°%d : Erreur lors de l'attachement de la socket au port %hu. Cause : ", myID, myPort);
+        perror("Bind()");
+        exit(1);
+    }
+
+    // on met notre socket en écoute de connexions 
+    if (listen(sockfd, 20) < 0){
+        printf("Erreur : Erreur lors de la mise en écoute. Cause : ");
+        perror("Listen()");
+        exit(EXIT_FAILURE);
+    }
+
+    return sockfd;
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -48,5 +88,27 @@ int main(int argc, char *argv[])
 
     return 0;
 }
+
+
+// Test de la fct° socketReceive pour bon fonctionnement
+// int main(int argc, char const *argv[])
+// {
+// unsigned short port; 
+// int siteNb;  
+
+// if (argc != 3)
+//     {
+//         fprintf(stderr, "Usage: %s numberSite, port\n", argv[0]);
+//         exit(1);
+//     }
+
+//     siteNb = atoi(argv[1]);
+//     port = (unsigned short) atoi(argv[2]);
+    
+//     int sfd = creerSocketReceveuse(siteNb, port);
+//     printf("\nFile descriptor returned: %d", sfd);
+
+
+// }
 
 
